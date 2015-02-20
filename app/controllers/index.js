57bcdc0ff3e5f6038e5b70f9f479bbf8f8b0de6c
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-02-20 10:22:10 +0100

    db added

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,26 @@
-function doClick(e) {
-    alert($.label.text);
-}
+// var hotspot_trailsCollection = Alloy.Collections.hotspot_trailsModel;
+// var imageCollection = Alloy.Collections.imageModel;
+// var infospot_trailsCollection = Alloy.Collections.infospot_trailsModel;
+// var infospotCollection = Alloy.Collections.infospotModel;
+// 
+// 
+// //--------------------------------------------------------------------
+// //Visa hotspots i tableView
+// 
+
+ // var hotspotCollection = Alloy.Collections.hotspotModel2;
+ // hotspotCollection.fetch();
+//  
+//  
+ // var data = [];
+ // hotspotCollection.map(function(hotspotModel2) {
+     // var title = hotspotModel2.get('name');
+     // var row = Ti.UI.createTableViewRow({"title":title});
+     // data.push(row);
+ // });
+// 
+// $.table.setData(data);
+
 
 $.index.open();
+
