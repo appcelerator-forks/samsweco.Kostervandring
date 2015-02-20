92fb330343ac0cf6e79272897e1fa965db245e5e
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-02-20 15:42:03 +0100

    projektet ihop

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,12 @@
 
-var hotspotCollection = Alloy.Collections.hotspotModel;
-hotspotCollection.fetch();
-
 function nextPage(){
 	var trails = Alloy.createController("trails").getView();
 	trails.open();
+}
 
+function hotspotView(){
+	var hotspots = Alloy.createController("hotspots").getView();
+	hotspots.open();
 }
 
 $.index.open();
