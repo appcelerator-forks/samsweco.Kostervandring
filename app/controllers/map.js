04440e5001b73b9c9481a8f40559dbe8a7f9d29e
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-02-26 16:12:43 +0100

    map med punkter och routes added

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -1,19 +1,70 @@
 var args = arguments[0] || {};
-
+	showMap();
 //Funktionerna nedan skickar användaren till rätt sida utifrån navigeringsmeyn
 function goHome(e){
 	$.map.close();
 }
 
-// var bb1 = Titanium.UI.createButtonBar({
-// labels:['One', 'Two', 'Three'],
-// backgroundColor:'#336699',
-// style:Titanium.UI.iPhone.SystemButtonStyle.BAR,
-// height:25,
-// width:200
-// });
-// 
-// $.map.add(bb1);
-//--------------------------------------------------------
+function showMap(){	  
+//Titanium.API.info()
+var MapModule = require('ti.map');
 
-$.map.open();
+var hamn = MapModule.createAnnotation({
+	latitude : 58.893550,
+	longitude : 11.048776,
+	title : 'Hamn',
+	subtitle : 'Hamnen, här kan du åka båt'
+});
+
+var toalett = MapModule.createAnnotation({
+	latitude : 58.891300,
+	longitude : 11.040732,
+	title : 'Toalett',
+	subtitle : 'Toalett, här kan du...'
+});
+
+var lekpark = MapModule.createAnnotation({
+	//58.894115, 11.040388
+	latitude : 58.894115,
+	longitude : 11.040388,
+	title : 'Lekpark',
+	subtitle : 'Lekpark, här kan du leka!'
+});
+
+var map3 = MapModule.createView({
+    userLocation: true,
+    mapType: MapModule.SATELLITE_TYPE,
+    animate: true,
+    //58.893539, 11.012579
+    region: {latitude: 58.893539, 
+    		longitude: 11.012579, 
+    		latitudeDelta: 0.1, 
+    		longitudeDelta: 0.1 },
+    annotations : [hamn, toalett, lekpark],		
+    height: '85%',
+    width: Ti.UI.FILL
+});
+
+var redroute = MapModule.createRoute({
+	width : 4,
+	color : 'red',
+	points : [
+	{latitude : hamn.latitude, longitude : hamn.longitude},
+	{latitude : toalett.latitude, longitude : toalett.longitude},
+	{latitude : lekpark.latitude, longitude : lekpark.longitude}]
+});
+
+var blueroute = MapModule.createRoute({
+	width : 4,
+	color : 'blue',
+	points : [
+	{latitude : 58.885401, longitude : 11.011292},
+	{latitude : 58.874577, longitude : 11.032063},
+	{latitude : 58.883538, longitude : 11.042706}]
+});
+
+map3.addRoute(blueroute);
+map3.addRoute(redroute);
+
+$.mapView.add(map3);
+};
