8e8838bccacba8177ed00777284b44bdda5bd27b
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-03-03 09:20:42 +0100

    lite mer layout

--- app/controllers/trailDetail.js
+++ app/controllers/trailDetail.js
@@ -1,7 +1,7 @@
 var args = arguments[0] || {};
 
 $.lblTrailName.text = args.title || 'Default Name';
-$.lblTrailLength.text = args.length || 'Default Length';
+$.lblTrailLength.text = "Distans: "+ args.length || 'Default Length';
 $.lblTrailInfo.text = args.infoTxt || 'Default infoText';
 $.lblTrailColor.text = args.color || 'Default Color';
 
