3385fbc0673e3579eea352fe7452df12e83f7fad
supersandra <svictorin@live.se>
2015-03-03 14:50:01 +0100

    Widgetpill

--- app/widgets/alertWidget/controllers/widget.js
+++ app/widgets/alertWidget/controllers/widget.js
@@ -9,3 +9,47 @@ function closeDialog(){
 function doClick(e){
     Ti.API.info('e.text: ' + e.text);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+function showLoc(myPos){	
+	var loc = Ti.Geolocation.getCurrentPosition(getLoc);
+	
+	myPos[loc];
+	
+	var latitude = myPos[0];
+	var longitude = myPos[1];
+	
+	Ti.API.info(latitude, longitude);
+}
+
+function getLoc(e){
+    var place;
+    
+    if(e.success){
+        Ti.API.info("Hittade dig!");
+        
+        var lat = e.coords.latitude;
+    	var lon = e.coords.longitude;
+    
+    	place = [lat, lon];
+    
+    	Ti.API.info(place);
+    } else {
+    	alert("Tusan");
+    }   
+    
+	return place;	
+};
+
+
+
