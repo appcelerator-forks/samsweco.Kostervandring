189dc044a36b6384584e2b6f7c1ed39feda3b5fd
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-03-04 10:09:27 +0100

    Bildspelet fixat!!!

--- app/controllers/trailDetail.js
+++ app/controllers/trailDetail.js
@@ -6,82 +6,21 @@ $.lblTrailColor.text = "Färgmarkering: " + args.color || 'Default Color';
 $.lblTrailInfo.text = "Beskrivning : " + args.infoTxt || 'Default infoText';
 
 var trailId = args.id;
-Titanium.API.info(JSON.stringify(trailId) + " är ID");
 
 selectTrailPics();
-//testArray();
-
-// function testArray(){
-	// var people = [
-	// {
-		// name : 'Anna',
-		// town : 'Nora'
-	// },
-// 	
-	// {
-		// name : 'Madde',
-		// town : 'Örebro'
-	// },
-// 	
-	// {
-		// name : 'Sandra',
-		// town : 'Örebro'
-	// }];
-// 	
-	// var results = [];
-	// people.forEach(function(person){
-		// results.push(person.name);
-	// });
-// 	
-	// Titanium.API.info(JSON.stringify(results));
-// }
 
 function selectTrailPics(){
 	
-	
-	//DETTA EXEMPEL FUNKAR! GRRRRRRRRRRRRRRRRRRRRRRR!!!!!!!
-		// var people = [
-	// {
-		// name : 'Anna',
-		// town : 'Nora'
-	// },
-// 	
-	// {
-		// name : 'Madde',
-		// town : 'Örebro'
-	// },
-// 	
-	// {
-		// name : 'Sandra',
-		// town : 'Örebro'
-	// }];
-// 	
-	// var results = [];
-	// people.forEach(function(person){
-		// results.push(person.name);
-	// });
-// 	
-	// Titanium.API.info(JSON.stringify(results));
-
-	// for(var i = 0; i < results.length; i++){
-		// var view_args = {
-		 // backgroundImage : results[i]
-	// };
-	// Titanium.API.info(JSON.stringify(results[i]));
-
 	var id = trailId;
 	var mediaCollection = Alloy.Collections.mediaModel;
 	mediaCollection.fetch({query: 'SELECT filename from mediaModel where trail_id="'+trailId+'"'});
-	//mediaCollection.fetch();
-
-	// var filename = mediaCollection.fetch('filename');
-	 Titanium.API.info(JSON.stringify(mediaCollection));
 	
-	for(var i = 0; i < mediaCollection.length; i++){	
+	
+	var jsonObj = mediaCollection.toJSON();	
+	for(var i = 0; i < jsonObj.length; i++){
 		var view_args = {
-		 backgroundImage : mediaCollection[0]
+		 backgroundImage : 'pics/'+jsonObj[i].filename
 	};
-	Titanium.API.info(JSON.stringify(mediaCollection[i]));
 	
 	var img_view = Ti.UI.createView(view_args);
 	
@@ -90,4 +29,3 @@ function selectTrailPics(){
 }
 
 $.trailDetail.open();
-//$.index.close();
