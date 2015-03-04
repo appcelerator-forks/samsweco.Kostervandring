e22ce17980970e43f274bece040cabffbd3d9f76
supersandra <svictorin@live.se>
2015-03-04 10:30:37 +0100

    fix

--- app/widgets/alertWidget/controllers/widget.js
+++ app/widgets/alertWidget/controllers/widget.js
@@ -4,7 +4,7 @@ function showDialog(){
 
 function closeDialog(){
 	$.alertView.visible = false;
-}
+};
 
 function doClick(e){
     Ti.API.info('e.text: ' + e.text);
@@ -15,40 +15,58 @@ function doClick(e){
 
 
 
+var latitude = " ";
+var longitude = " ";
 
-
-
-
-
-
-
-
-// function getLoc(e){
-    // var place;
-//     
-    // if(e.success){
+// function getLoc(e){    
+// 
+    	// if(e.success){
         // Ti.API.info("Hittade dig!");
-//         
+        // Ti.API.info(e);        
+       // }
         // latitude = e.coords.latitude;
     	// longitude = e.coords.longitude;
 //     
-    	// // place = [lat, lon];
-//     
     	// Ti.API.info(latitude, longitude);
-    // } else {
-    	// alert("Tusan");
-    // }  
-//     
-    // // return place;
 // };
 
-//var loc = Ti.Geolocation.getCurrentPosition(getLoc);
+Ti.Geolocation.getCurrentPosition(getLat);
+Ti.Geolocation.getCurrentPosition(getLon);
 
-function showLoc(){	
-	
-	// var lat = Alloy.Globals.latitude;
-	// var lon = Alloy.Globals.longitude;
+
+function getLat(e){
+	if(e.success){
+        Ti.API.info("Hittade dig!");
+        Ti.API.info(e);        
+      }
+      
+        latitude = e.coords.latitude;
+    
+    	Ti.API.info(latitude);
+    	$.lblLat.text = latitude;
+};
+
+function getLon(e){
+	if(e.success){
+        Ti.API.info("Hittade dig!");
+        Ti.API.info(e);        
+      }
+      
+        longitude = e.coords.longitude;
+    
+    	Ti.API.info(longitude);
+    	$.lblLon.text = longitude;
+};
+
+// function writePlace(){
+	// var lati = Ti.Geolocation.getCurrentPosition(getLat);
+	// var longi = Ti.Geolocation.getCurrentPosition(getLon);
 // 	
-	// Ti.API.info(Alloy.Globals.getLoc());
-}
+// 	
+// 	
+// }
+
+
+
+
 
