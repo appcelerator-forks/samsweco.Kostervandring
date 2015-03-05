3a5d0b25d058986bea04e747e767ea8cafa3d485
madelenejohnsson <madelenjohnsson@hotmail.com>
2015-03-05 13:57:59 +0100

    optionChoice in Quiz, can still check all.. next problem!

--- app/controllers/quizDetail.js
+++ app/controllers/quizDetail.js
@@ -3,3 +3,17 @@ var args = arguments[0] || {};
 var quizCollection = Alloy.Collections.quizModel;
 quizCollection.fetch();
 
+// <!-- controllers/index.js -->
+$.lista.addEventListener('itemclick', function(e){
+    var item = $.section.getItemAt(e.itemIndex);
+    if (item.properties.accessoryType == Ti.UI.LIST_ACCESSORY_TYPE_NONE) {
+        item.properties.accessoryType = Ti.UI.LIST_ACCESSORY_TYPE_CHECKMARK;
+        item.properties.color = 'black';
+    }
+    else {
+        item.properties.accessoryType = Ti.UI.LIST_ACCESSORY_TYPE_NONE;
+        item.properties.color = 'black';
+    }
+    $.section.updateItemAt(e.itemIndex, item);
+});
+
--- app/widgets/alertWidget/controllers/widget.js
+++ app/widgets/alertWidget/controllers/widget.js
@@ -30,33 +30,33 @@ var longitude = " ";
     	// Ti.API.info(latitude, longitude);
 // };
 
-Ti.Geolocation.getCurrentPosition(getLat);
-Ti.Geolocation.getCurrentPosition(getLon);
-
-
-function getLat(e){
-	if(e.success){
-        Ti.API.info("Hittade dig!");
-        Ti.API.info(e);        
-      }
-      
-        latitude = e.coords.latitude;
-    
-    	Ti.API.info(latitude);
-    	$.lblLat.text = latitude;
-};
-
-function getLon(e){
-	if(e.success){
-        Ti.API.info("Hittade dig!");
-        Ti.API.info(e);        
-      }
-      
-        longitude = e.coords.longitude;
-    
-    	Ti.API.info(longitude);
-    	$.lblLon.text = longitude;
-};
+// Ti.Geolocation.getCurrentPosition(getLat);
+// Ti.Geolocation.getCurrentPosition(getLon);
+// 
+// 
+// function getLat(e){
+	// if(e.success){
+        // Ti.API.info("Hittade dig!");
+        // Ti.API.info(e);        
+      // }
+//       
+        // latitude = e.coords.latitude;
+//     
+    	// Ti.API.info(latitude);
+    	// $.lblLat.text = latitude;
+// };
+// 
+// function getLon(e){
+	// if(e.success){
+        // Ti.API.info("Hittade dig!");
+        // Ti.API.info(e);        
+      // }
+//       
+        // longitude = e.coords.longitude;
+//     
+    	// Ti.API.info(longitude);
+    	// $.lblLon.text = longitude;
+// };
 
 // function writePlace(){
 	// var lati = Ti.Geolocation.getCurrentPosition(getLat);
