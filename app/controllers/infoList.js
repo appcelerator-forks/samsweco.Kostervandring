b4c26fdda501941e17ec56fc5862e324a35b86b9
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-03-10 14:36:04 +0100

    infoList & detail

--- app/controllers/infoList.js
+++ app/controllers/infoList.js
@@ -4,14 +4,14 @@ var infoCollection = Alloy.Collections.infoModel;
 infoCollection.fetch();
 
 function showinfoDetails(info) {
-	//Titanium.API.info(info.name);
+	Titanium.API.info(info.name);
 	var selectedInfo = info.row;
 	var args = {
 		name : selectedInfo.name,
 		infoTxt : selectedInfo.infoTxt
 	};
 
-	var hotspotDetail = Alloy.createController("infoDetail", args).getView();
+	var infoDetail = Alloy.createController("infoDetail", args).getView();
 	infoDetail.open();
 	$.infoList.close();
 }
