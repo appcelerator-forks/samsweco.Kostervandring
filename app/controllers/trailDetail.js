2d0cdbb4a74002641065e51121a69a91f83ae7bc
supersandra <svictorin@live.se>
2015-03-11 11:16:36 +0100

    funkar
    visar dock bara sista

--- app/controllers/trailDetail.js
+++ app/controllers/trailDetail.js
@@ -29,43 +29,49 @@ function selectTrailPics() {
 	}
 }
 
+function showHotspots() {
+	var data = getHotspotData();
 
-
-
-
-
-//ERROR: message = "undefined is not an object (evaluating 'hotstrailCollection.fetch')";
-
-function showHotspots(){
-	var id = getHotspotId();
-	var hotCollection = Alloy.Collections.hotspotModel;
-	hotCollection.fetch({
-		query: 'SELECT name, cover_pic from hotspotModel where id="' + id + '"'
-	});
-	var hotTrails = hotCollection.toJSON();
-	for (var i = 0; i < hotTrails.length; i++) {
-		//var view_args = {
-			$.listImageview.image = '/pics/' + hotTrails[i].cover_pic;
-			$.lblHotName.text = hotTrails[i].name;
-			
-			//text : name,
-			//image : '/pics/' + hotTrails[i].cover_pic
-		};
-}
-// 
-function getHotspotId(){
-	// var id = trailId;
-	// var hotstrailCollection = Alloy.Collections.hotspot_trailModel;
-	// hotstrailCollection.fetch({
-		// query: 'SELECT hotspotID from hotspot_trailModel where trailsID="' + trailId + '"'
+	// var hotCollection = Alloy.Collections.hotspotModel;
+	// hotCollection.fetch({
+	// query : 'SELECT name, cover_pic from hotspotModel where id="' + id + '"'
 	// });
-// 	
-	// var hotTrails = hotstrailCollection.toJSON();
-// 	
-	// Ti.API.info(hotTrails);
-	return 3;
-	//	return hotTrails;
+
+	// var hotTrails = data.toJSON();
+	
+	for (var i = 0; i < data.length; i++) {
+		$.listImageview.image = '/pics/' + data[i].cover_pic; 
+		$.lblHotName.text = data[i].name;
+	};
+	
+	alert(data);
 }
 
+// function getHotspotId() {
+// var id = trailId;
+// Ti.API.info(id);
+//
+// var hotstrailCollection = Alloy.Collections.hotspot_trailsModel;
+// hotstrailCollection.fetch({
+// query : 'SELECT hotspotID from hotspot_trailsModel where trailsID="' + trailId + '"'
+// });
+//
+// var hotTrails = hotstrailCollection.toJSON();
+//
+// Ti.API.info(hotTrails);
+// // return hotTrails;
+// }
+
+function getHotspotData() {
+	var id = trailId;
+
+	var hotstrailCollection = Alloy.Collections.hotspotModel;
+	hotstrailCollection.fetch({
+		query : 'SELECT hotspotModel.name, hotspotModel.cover_pic from hotspotModel join hotspot_trailsModel on hotspotModel.id = hotspot_trailsModel.hotspotID where trailsID ="' + id + '"'
+	});
+
+	var hotTrails = hotstrailCollection.toJSON();
+	return hotTrails;
+}
 
 $.trailDetail.open();
