9f6a32ed9903502548efd1f5fad2eafc284e8aab
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-03-11 08:04:00 +0100

    webview

--- app/controllers/infoDetail.js
+++ app/controllers/infoDetail.js
@@ -4,10 +4,19 @@ var args = arguments[0] || {};
 $.lblInfoTitle.text = args.name || "Title";
 $.lblInfoText.text = args.infoTxt || "Info";
 $.lblInfoLink.text = args.link || "url";
-
 $.infoImg.image = args.img;
+
+var url = args.link;
 
 var link = $.lblInfoLink;
 link.addEventListener('click', function(e) {
-    Titanium.Platform.openURL(args.link);
+    // Titanium.Platform.openURL(args.link);
+    openLink(url);
 });
+
+function openLink(link){
+  var webview = Titanium.UI.createWebView({url:link});
+    var window = Titanium.UI.createWindow();
+    window.add(webview);
+    window.open({modal:true});
+   }
