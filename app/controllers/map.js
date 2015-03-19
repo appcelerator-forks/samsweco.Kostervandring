2b41dbfd6373b496ad4a858893092e3a88c1517d
supersandra <svictorin@live.se>
2015-03-19 13:38:04 +0100

    geoJSON stuff

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -1,21 +1,52 @@
 var args = arguments[0] || {};
-
+var route;
 var radius = 10;
 var map3;
 var MapModule = require('ti.map');
 showMap();
-addRoutes();
+//createMapRoutes();
+// addRoutes();
 
+function createMapRoutes() {
 
-function goHome(e) {
-	$.map.close();
+//FUNKAR EJ, går ej in i loopen, se alerts... vill skapa en array med geometries och sedan loopan genom dem?
+//Som det är nu kommer den ju bara ta första geometry'n eftersom indexet styrs av u, som aldrig blir mer än 0 eftersom det 
+//bara finns ett objekt som loopas...?
+	var fu = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory + "rutter/" + "adventureroute.json").read().text;
+	var v = JSON.parse(fu);
+	var geoArray = [];
+	alert("i create map!");
+	for (var u = 0; u < v.length; u++) {
+		alert("i loopen!");
+		var coords = v[u].features[0].geometry[u].paths;
+		var j = new Array();
+
+		for (var i = 0; i < coords.length; i++) {
+			var c = {
+				latitude : coords[i][1],
+				longitude : coords[i][0]
+			};
+			
+			j.push(c);
+		}
+
+		var route = {
+			name : "Vandringsleder",
+			points : j,
+			color : "red",
+			width : 3
+		};
+		
+		map3.addRoute(MapModule.createRoute(route));
+		// mapview.addRoute(Map.createRoute(route));
+	}
 }
 
 Ti.Geolocation.getCurrentPosition(function(e) {
 	if (e.error) {
 		alert('Get current position' + e.error);
 	} else {
-		Ti.API.info(JSON.stringify(e.coords));
+		// Ti.API.info(JSON.stringify(e.coords));
 	}
 });
 
@@ -28,7 +59,7 @@ if (Ti.Geolocation.locationServicesEnabled) {
 		if (e.error) {
 			alert('Add eventlistener!' + e.error);
 		} else {
-		//	Ti.API.info(JSON.stringify(e.coords));
+			//	Ti.API.info(JSON.stringify(e.coords));
 			getPosition(e.coords);
 		}
 	});
@@ -156,221 +187,221 @@ function showMap() {
 	$.mapView.add(map3);
 };
 
-$.btnNormal.addEventListener('click', function(){
+$.btnNormal.addEventListener('click', function() {
 	map3.mapType = MapModule.NORMAL_TYPE;
 });
 
-$.btnHybrid.addEventListener('click', function(){
+$.btnHybrid.addEventListener('click', function() {
 	map3.mapType = MapModule.HYBRID_TYPE;
 });
 
-$.btnSatellit.addEventListener('click', function(){
+$.btnSatellit.addEventListener('click', function() {
 	map3.mapType = MapModule.SATELLITE_TYPE;
 });
 
-function addRoutes(){
-	addYellowRoute();
-	addGreenRoute();
-	addOrangeRoute();
-}
-
-function addYellowRoute() {
-	var testPoints = [{
-		latitude : 58.891340,
-		longitude : 11.008121
-	}, {
-		latitude : 58.890869,
-		longitude : 11.008969
-	}, {
-		latitude : 58.890326,
-		longitude : 11.009591
-	}, {
-		latitude : 58.889162,
-		longitude : 11.009666
-	}, {
-		latitude : 58.888940,
-		longitude : 11.009816
-	}, {
-		latitude : 58.888264,
-		longitude : 11.010717
-	}, {
-		latitude : 58.887954,
-		longitude : 11.010899
-	}, {
-		latitude : 58.887699,
-		longitude : 11.010717
-	}, {
-		latitude : 58.887488,
-		longitude : 11.010856
-	}, {
-		latitude : 58.887061,
-		longitude : 11.011607
-	}, {
-		latitude : 58.887011,
-		longitude : 11.012176
-	}, {
-		latitude : 58.886856,
-		longitude : 11.012680
-	}, {
-		latitude : 58.886457,
-		longitude : 11.013131
-	}, {
-		latitude : 58.886224,
-		longitude : 11.013313
-	}, {
-		latitude : 58.885681,
-		longitude : 11.012122
-	}, {
-		latitude : 58.884783,
-		longitude : 11.006071
-	}];
-
-	var route = MapModule.createRoute({
-		points : testPoints,
-		color : "yellow",
-		width : 2
-	});
-
-	map3.addRoute(route);
-}
-
-function addGreenRoute() {
-	var testPoints = [{
-		latitude : 58.880741,
-		longitude : 11.008442
-	}, {
-		latitude : 58.880286,
-		longitude : 11.011231
-	}, {
-		latitude : 58.880541,
-		longitude : 11.014085
-	}, {
-		latitude : 58.882116,
-		longitude : 11.018226
-	}, {
-		latitude : 58.882970,
-		longitude : 11.022314
-	}, {
-		latitude : 58.884245,
-		longitude : 11.027818
-	}, {
-		latitude : 58.884993,
-		longitude : 11.029556
-	}, {
-		latitude : 58.885331,
-		longitude : 11.030189
-	}, {
-		latitude : 58.884976,
-		longitude : 11.033032
-	}, {
-		latitude : 58.885453,
-		longitude : 11.034459
-	}, {
-		latitude : 58.885558,
-		longitude : 11.035457
-	}, {
-		latitude : 58.884865,
-		longitude : 11.039491
-	}, {
-		latitude : 58.884028,
-		longitude : 11.040559
-	}, {
-		latitude : 58.882553,
-		longitude : 11.040103
-	}, {
-		latitude : 58.882159,
-		longitude : 11.039390
-	}, {
-		latitude : 58.881383, 
-		longitude : 11.039175
-	}, {
-		latitude : 58.880540, 
-		longitude : 11.038317
-	}, {
-		latitude : 58.878588, 
-		longitude : 11.040977 
-	}, {
-		latitude : 58.878389, 
-		longitude : 11.045269
-	}, {
-		latitude : 58.879459, 
-		longitude : 11.045811
-	}];
-
-	var route = MapModule.createRoute({
-		points : testPoints,
-		color : "green",
-		width : 2
-	});
-
-	map3.addRoute(route);
-}
-
-function addOrangeRoute() {
-	var testPoints = [{
-		latitude : 58.897060, 
-		longitude : 11.005058
-	}, {
-		latitude : 58.897725, 
-		longitude : 11.003856
-	}, {
-		latitude : 58.898169, 
-		longitude : 11.001882
-	}, {
-		latitude : 58.899499, 
-		longitude : 10.997676
-	}, {
-		latitude : 58.901272,
-		longitude : 10.993728
-	}, {
-		latitude : 58.903067,
-		longitude : 10.992424
-	}, {
-		latitude : 58.905112,
-		longitude : 10.991496
-	}, {
-		latitude : 58.905865, 
-		longitude : 10.992440
-	}, {
-		latitude : 58.906619,
-		longitude : 10.994157
-	}, {
-		latitude : 58.909057,
-		longitude : 10.997332
-	}, {
-		latitude : 58.910376,
-		longitude : 11.000481
-	}, {
-		latitude : 58.910354,
-		longitude : 11.001431
-	}, {
-		latitude : 58.909966,
-		longitude : 11.001769
-	}, {
-		latitude : 58.908043,
-		longitude : 11.005272
-	}, {
-		latitude : 58.905710,
-		longitude : 11.006651
-	}, {
-		latitude : 58.903455,
-		longitude : 11.006313 
-	}, {
-		latitude : 58.901903,  
-		longitude : 11.008287
-	}, {
-		latitude : 58.899509, 
-		longitude :  11.007944 
-	}, {
-		latitude : 58.897159, 
-		longitude : 11.005197
-	}];
-
-	var route = MapModule.createRoute({
-		points : testPoints,
-		color : "orange",
-		width : 2
-	});
-
-	map3.addRoute(route);
-}
+// function addRoutes(){
+// addYellowRoute();
+// addGreenRoute();
+// addOrangeRoute();
+// }
+//
+// function addYellowRoute() {
+// var testPoints = [{
+// latitude : 58.891340,
+// longitude : 11.008121
+// }, {
+// latitude : 58.890869,
+// longitude : 11.008969
+// }, {
+// latitude : 58.890326,
+// longitude : 11.009591
+// }, {
+// latitude : 58.889162,
+// longitude : 11.009666
+// }, {
+// latitude : 58.888940,
+// longitude : 11.009816
+// }, {
+// latitude : 58.888264,
+// longitude : 11.010717
+// }, {
+// latitude : 58.887954,
+// longitude : 11.010899
+// }, {
+// latitude : 58.887699,
+// longitude : 11.010717
+// }, {
+// latitude : 58.887488,
+// longitude : 11.010856
+// }, {
+// latitude : 58.887061,
+// longitude : 11.011607
+// }, {
+// latitude : 58.887011,
+// longitude : 11.012176
+// }, {
+// latitude : 58.886856,
+// longitude : 11.012680
+// }, {
+// latitude : 58.886457,
+// longitude : 11.013131
+// }, {
+// latitude : 58.886224,
+// longitude : 11.013313
+// }, {
+// latitude : 58.885681,
+// longitude : 11.012122
+// }, {
+// latitude : 58.884783,
+// longitude : 11.006071
+// }];
+//
+// var route = MapModule.createRoute({
+// points : testPoints,
+// color : "yellow",
+// width : 2
+// });
+//
+// map3.addRoute(route);
+// }
+//
+// function addGreenRoute() {
+// var testPoints = [{
+// latitude : 58.880741,
+// longitude : 11.008442
+// }, {
+// latitude : 58.880286,
+// longitude : 11.011231
+// }, {
+// latitude : 58.880541,
+// longitude : 11.014085
+// }, {
+// latitude : 58.882116,
+// longitude : 11.018226
+// }, {
+// latitude : 58.882970,
+// longitude : 11.022314
+// }, {
+// latitude : 58.884245,
+// longitude : 11.027818
+// }, {
+// latitude : 58.884993,
+// longitude : 11.029556
+// }, {
+// latitude : 58.885331,
+// longitude : 11.030189
+// }, {
+// latitude : 58.884976,
+// longitude : 11.033032
+// }, {
+// latitude : 58.885453,
+// longitude : 11.034459
+// }, {
+// latitude : 58.885558,
+// longitude : 11.035457
+// }, {
+// latitude : 58.884865,
+// longitude : 11.039491
+// }, {
+// latitude : 58.884028,
+// longitude : 11.040559
+// }, {
+// latitude : 58.882553,
+// longitude : 11.040103
+// }, {
+// latitude : 58.882159,
+// longitude : 11.039390
+// }, {
+// latitude : 58.881383,
+// longitude : 11.039175
+// }, {
+// latitude : 58.880540,
+// longitude : 11.038317
+// }, {
+// latitude : 58.878588,
+// longitude : 11.040977
+// }, {
+// latitude : 58.878389,
+// longitude : 11.045269
+// }, {
+// latitude : 58.879459,
+// longitude : 11.045811
+// }];
+//
+// var route = MapModule.createRoute({
+// points : testPoints,
+// color : "green",
+// width : 2
+// });
+//
+// map3.addRoute(route);
+// }
+//
+// function addOrangeRoute() {
+// var testPoints = [{
+// latitude : 58.897060,
+// longitude : 11.005058
+// }, {
+// latitude : 58.897725,
+// longitude : 11.003856
+// }, {
+// latitude : 58.898169,
+// longitude : 11.001882
+// }, {
+// latitude : 58.899499,
+// longitude : 10.997676
+// }, {
+// latitude : 58.901272,
+// longitude : 10.993728
+// }, {
+// latitude : 58.903067,
+// longitude : 10.992424
+// }, {
+// latitude : 58.905112,
+// longitude : 10.991496
+// }, {
+// latitude : 58.905865,
+// longitude : 10.992440
+// }, {
+// latitude : 58.906619,
+// longitude : 10.994157
+// }, {
+// latitude : 58.909057,
+// longitude : 10.997332
+// }, {
+// latitude : 58.910376,
+// longitude : 11.000481
+// }, {
+// latitude : 58.910354,
+// longitude : 11.001431
+// }, {
+// latitude : 58.909966,
+// longitude : 11.001769
+// }, {
+// latitude : 58.908043,
+// longitude : 11.005272
+// }, {
+// latitude : 58.905710,
+// longitude : 11.006651
+// }, {
+// latitude : 58.903455,
+// longitude : 11.006313
+// }, {
+// latitude : 58.901903,
+// longitude : 11.008287
+// }, {
+// latitude : 58.899509,
+// longitude :  11.007944
+// }, {
+// latitude : 58.897159,
+// longitude : 11.005197
+// }];
+//
+// var route = MapModule.createRoute({
+// points : testPoints,
+// color : "orange",
+// width : 2
+// });
+//
+// map3.addRoute(route);
+// }
