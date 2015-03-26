33de0a208a03e803982a627e3cc0fe82cc153d41
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-03-26 10:24:29 +0100

    mapDetail visar hotspot

--- app/controllers/mapDetail.js
+++ app/controllers/mapDetail.js
@@ -14,6 +14,7 @@ var MapModule = require('ti.map');
 
 showMap();
 createMapRoutes(getFile(), zoomName, zoomColor);
+addAnnotations();
 
 function showMap() {
 	try {
@@ -24,76 +25,51 @@ function showMap() {
 			region : {
 				latitude : 58.893539,
 				longitude : 11.012579,
-				latitudeDelta : 0.1,
-				longitudeDelta : 0.1
+				latitudeDelta : 0.03,
+				longitudeDelta : 0.03
 			},
 			height : '90%',
 			width : Ti.UI.FILL
 		});
 
-		var markers = Alloy.Collections.hotspotModel;
-
-		markers.fetch({
-			success : displayMarkers,
-			error : Ti.API.error
-		});
-
+		$.mapDetailView.add(zoomedMap);
 	} catch(e) {
 		newError("Något gick fel när sidan skulle laddas, prova igen!", "Map - showMap");
 	}
+}
 
-	function displayMarkers() {
-		try {
-			markers.each(function(marker) {
-				var markerAnnotation = MapModule.createAnnotation({
-					latitude : marker.get('xkoord'),
-					longitude : marker.get('ykoord'),
-					title : marker.get('name')
-				});
+function createMapRoutes(file, name, color) {
+	Ti.API.info(file);
 
-				zoomedMap.addAnnotation(markerAnnotation);
-			});
-		} catch(e) {
-			newError("Något gick fel när sidan skulle laddas, prova igen!", "Map - displayMarkers");
-		}
-	}
+	var zoomedRoute = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory + "/routes/" + file).read().text;
+	var v = JSON.parse(zoomedRoute);
 
+	var array = [];
+	array.push(v);
 
-	$.mapDetailView.add(zoomedMap);
-};
+	for (var u = 0; u < array.length; u++) {
+		var coords = array[0].features[0].geometry.paths[u];
 
-	function createMapRoutes(file, name, color) {
-		Ti.API.info(file);
-		
-		var zoomedRoute = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory + "/routes/" + file).read().text;
-		var v = JSON.parse(zoomedRoute);
+		var j = new Array();
 
-		var array = [];
-		array.push(v);
+		for (var i = 0; i < coords.length; i++) {
 
-		for (var u = 0; u < array.length; u++) {
-			var coords = array[0].features[0].geometry.paths[u];
-
-			var j = new Array();
-
-			for (var i = 0; i < coords.length; i++) {
-
-				var c = {
-					latitude : coords[i][1],
-					longitude : coords[i][0]
-				};
-				j.push(c);
-			}
-
-			var route = {
-				name : name,
-				points : j,
-				color : color,
-				width : '2dp'
+			var c = {
+				latitude : coords[i][1],
+				longitude : coords[i][0]
 			};
-			zoomedMap.addRoute(MapModule.createRoute(route));
+			j.push(c);
 		}
+
+		var route = {
+			name : name,
+			points : j,
+			color : color,
+			width : '2dp'
+		};
+		zoomedMap.addRoute(MapModule.createRoute(route));
 	}
+}
 
 function getFile() {
 	var jsonFileCollection = Alloy.Collections.jsonFilesModel;
@@ -103,6 +79,48 @@ function getFile() {
 
 	var filename = jsonFileCollection.toJSON();
 	return filename[0].filename;
+}
+
+function addAnnotations() {
+	var markerArray = [];
+	var markersCollection = Alloy.Collections.hotspotModel;
+	var idCollection = getID();
+
+	for (var i = 0; i < idCollection.length; i++) {
+		markersCollection.fetch({
+			query : 'SELECT name, ykoord, xkoord FROM hotspotModel where id="' + idCollection[i] + '"'
+		});
+
+		var markersJSON = markersCollection.toJSON();
+		for (var u = 0; u < markersJSON.length; u++) {
+			var marker = MapModule.createAnnotation({
+				latitude : markersJSON[u].ykoord,
+				longitude : markersJSON[u].xkoord,
+				title : markersJSON[u].name
+			});
+
+			markerArray.push(marker);
+		}
+	}
+	zoomedMap.addAnnotations(markerArray);
+}
+
+function getID() {
+	var idArray = [];
+
+	var hotspot_trailsCollection = Alloy.Collections.hotspot_trailsModel;
+	hotspot_trailsCollection.fetch({
+		query : 'SELECT hotspotID FROM hotspot_trailsModel WHERE trailsID ="' + zoomId + '"'
+	});
+
+	var jsonObj = hotspot_trailsCollection.toJSON();
+	Ti.API.info("Json obh : " + JSON.stringify(jsonObj));
+
+	for (var i = 0; i < jsonObj.length; i++) {
+		idArray.push(jsonObj[i].hotspotID);
+	}
+	Ti.API.info("IdArray: " + JSON.stringify(idArray));
+	return idArray;
 }
 
 $.btnNormal.addEventListener('click', function() {
