213e910ffb0d64ea495364ba2867ca69cd0ae5a4
madelenejohnsson <madelenjohnsson@hotmail.com>
2015-04-07 11:05:13 +0200

    bildtexter

--- app/controllers/hotspotDetail.js
+++ app/controllers/hotspotDetail.js
@@ -7,19 +7,20 @@ var hotspotId = args.id || "Id";
 var picId = args.filename || "filename";
 
 setPics();
-setPicText();
+//setPicText();
 
 //-----------------------------------------------------------
 // Sätter bilder till bildspelet
 //-----------------------------------------------------------
 function setPics() {
 	try {
-			selectHotspotPics();
-//		}
+		selectHotspotPics();
+		//		}
 	} catch(e) {
 		newError("Något gick fel när sidan skulle laddas, prova igen!", "HotspotDetail - setPics");
 	}
 }
+
 //-----------------------------------------------------------
 // Hämtar bilder för bildspelet
 //-----------------------------------------------------------
@@ -27,20 +28,51 @@ function selectHotspotPics() {
 	try {
 
 		var mediaCollection = getMediaCollection();
-			mediaCollection.fetch({
-		query : 'SELECT * FROM mediaModel WHERE hotspot_id = "' + hotspotId + '"'
-	});
+		mediaCollection.fetch({
+			query : 'SELECT * FROM mediaModel WHERE hotspot_id = "' + hotspotId + '"'
+		});
 		var jsonMedia = mediaCollection.toJSON();
-		
-		for (var i = 0; i < jsonMedia.length; i++) {
-			var view_args = {
-				backgroundImage : "/pics/" + jsonMedia[i].filename
-			};
 
-			var img_view = Ti.UI.createView(view_args);
-			$.slideShowHotspotDetail.addView(img_view);
+		for (var i = 0; i < jsonMedia.length; i++) {
+			// var view_args = {
+// 
+				// //imgTxt : jsonMedia[i].img_txt
+			// };
+
+			var img_view = Ti.UI.createView({
+				backgroundImage : "/pics/" + jsonMedia[i].filename,
+				height : '200dp',
+				width : '300dp',
+				top : '0dp'
+				});
+
+			var lblImgTxt = Ti.UI.createLabel({
+				left : '5dp',
+				top : '0dp',
+				text : jsonMedia[i].img_txt,
+				color : 'white',
+				font : {
+					fontSize : 12,
+					fontStyle : 'italic',
+					textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER
+				},
+				
+			});
+
+			var backgroundView = Ti.UI.createView({
+				layout : 'vertical',
+				backgroundColor : 'black',
+				height : Ti.UI.SIZE,
+				width : Ti.UI.SIZE
+			});
+
+			backgroundView.add(img_view);
+			backgroundView.add(lblImgTxt);
+			
+			$.slideShowHotspotDetail.addView(backgroundView);
+			//$.slideShowHotspotDetail.add(lblImgTxt);
 		}
-		
+
 	} catch(e) {
 		newError("Något gick fel när sidan skulle laddas, prova igen!", "HotspotDetail - selectHotspotPics");
 	}
@@ -49,19 +81,13 @@ function selectHotspotPics() {
 //---------------------------------------------------------
 //Sätter bildtexter till hotspotsen
 //---------------------------------------------------------
-function setPicText()
-{
+function setPicText() {
+
 	var textCollection = Alloy.Collections.mediaModel;
-	textCollection.fetch({
-		query : 'SELECT * FROM mediaModel where id = 9'
-	});
+	textCollection.fetch('SELECT img_txt FROM mediaModel where filename = anemon.png');
 
 	txt = textCollection.toJSON();
-	
-	imgTxt = txt[0].img_txt;
-	
-	$.lblImgTxt.text = imgTxt;
-//	Ti.API.info('HÄR ÄR INFORMATION SOM VI VILL SE : ' + imgTxt);
+	text = JSON.stringify(txt);
+	Ti.API.info('HÄR ÄR INFORMATION SOM VI VILL SE : ' + text);
 }
-
 
